6f87a0e63d4156dc48d7a539a1ff83ae693136c4
lquochieu <trantronghiep2001@gmail.com>
2023-11-30 13:43:22 +0700

    fix: merge

--- contracts/actions/trava/nft/heuristicFarming/helpers/MainnetTravaNFTHeuristicFarmingAddresses.sol
+++ contracts/actions/trava/nft/heuristicFarming/helpers/MainnetTravaNFTHeuristicFarmingAddresses.sol
@@ -3,5 +3,5 @@
 pragma solidity ^0.8.4;
 
 contract MainnetTravaNFTHeuristicFarmingAddresses {
-    address internal constant NFT_COLLECTION = 0x5D996eC57756cEB127a4eD3302d7F28F52FDEbb1;
+    address internal constant NFT_COLLECTION = 0x9C3E857eCe6224544aA77c1A6e500d8Fa1c9C102;
 }
--- contracts/actions/trava/nft/marketplace/veTrava/helpers/MainnetTravaNFTVeTravaAddresses.sol
+++ contracts/actions/trava/nft/marketplace/veTrava/helpers/MainnetTravaNFTVeTravaAddresses.sol
@@ -3,6 +3,6 @@
 pragma solidity ^0.8.4;
 
 contract MainnetTravaNFTVeTravaAddresses {
-    address internal constant VE_TRAVA_ADDRESS = 0x7E41803de7781f53D1901A3d70A3D3747b3B3B63;
-    address internal constant VE_TRAVA_MARKETPLACE_ADDRESS = 0x642e9fD16cA66db2747695dc0DddD0ce54aB3AB1;
+    address internal constant VE_TRAVA_ADDRESS = 0xedf6a93772EEcFB1300F61F6C1aE9680c33996A2;
+    address internal constant VE_TRAVA_MARKETPLACE_ADDRESS = 0x65c62822940C1a57C90Ac8BF0874730624ec7Fb7;
 }
